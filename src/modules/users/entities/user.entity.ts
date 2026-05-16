aac29ed52c9812aa9cd35faa5af5beea6ed54fa5
Pradixx <dieguittosilva09@gmail.com>
2026-05-16 19:21:43 -0300

    feat: adiciona entidade User

---
+++ src/modules/users/entities/user.entity.ts
@@ -0,0 +1,32 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+  OneToMany,
+} from 'typeorm';
+
+@Entity('users')
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ length: 100 })
+  name: string;
+
+  @Column({ unique: true, length: 150 })
+  email: string;
+
+  @Column()
+  password: string;
+
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt: Date;
+
+  @UpdateDateColumn({ name: 'updated_at' })
+  updatedAt: Date;
+
+  @OneToMany('Transaction', 'user')
+  transactions: any[];
+}
